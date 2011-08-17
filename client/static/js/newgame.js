7907ccec4759df6d9884028ff8aa6f590a889638
Yann Coupin <yann@coupin.net>
2011-08-17 22:49:53 +0200

    Make the JS work both on a desktop and in Safari on iOS

--- client/static/js/newgame.js
+++ client/static/js/newgame.js
@@ -1,50 +1,42 @@
 $(document).ready(function(){
     $('input[name="team_a"]').click(function(event){
-        if ($('input:checked[name="team_a"]').length == 3)
+        me = event.target
+        if ($('input:checked[name="team_a"]').length == 3 || $(me.nextSibling).hasClass("disabled"))
         {
             event.preventDefault();
             return;
         }
-        me = event.target
         alter_ego_label = $('input[name="team_b"][value="'+me.value+'"] + label');
         alter_ego = $('input[name="team_b"][value="'+me.value+'"]');
         if (me.checked)
         {
             alter_ego_label.addClass('disabled');
-            alter_ego_label.click(false);
-            alter_ego.click(false);
         }
         else
         {
             alter_ego_label.removeClass('disabled');
-            alter_ego_label.unbind('click', false);
-            alter_ego.unbind('click', false);
         }
     });
-    //$('input[name="team_a"] + label').click(function(event){event.target.previousSibling.click();event.preventDefault()});
+    $('input[name="team_a"] + label').click(function(event){event.target.previousSibling.click();event.preventDefault()});
     $('input[name="team_b"]').click(function(event){
-        if ($('input:checked[name="team_b"]').length == 3)
+        me = event.target
+        if ($('input:checked[name="team_b"]').length == 3 || $(me.nextSibling).hasClass("disabled"))
         {
             event.preventDefault();
             return;
         }
-        me = event.target
         alter_ego_label = $('input[name="team_a"][value="'+me.value+'"] + label');
         alter_ego = $('input[name="team_a"][value="'+me.value+'"]');
         if (me.checked)
         {
             alter_ego_label.addClass('disabled');
-            alter_ego_label.click(false);
-            alter_ego.click(false);
         }
         else
         {
             alter_ego_label.removeClass('disabled');
-            alter_ego_label.unbind('click', false);
-            alter_ego.unbind('click', false);
         }
     });
-    //$('input[name="team_b"] + label').click(function(event){event.target.previousSibling.click();event.preventDefault()});
+    $('input[name="team_b"] + label').click(function(event){event.target.previousSibling.click();event.preventDefault()});
     $('form').submit(function(){
         a = $('input:checked[name="team_a"]').length;
         b = $('input:checked[name="team_b"]').length;
